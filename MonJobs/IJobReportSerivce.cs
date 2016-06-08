ae053ce1640db124fd6d0601e4040247bbd1669d
Mitch Ferrer <mitch@lifeafternormal.net>
2016-06-07 20:07:30 -0600

    adding detail to job report service

--- MonJobs/IJobReportSerivce.cs
+++ MonJobs/IJobReportSerivce.cs
@@ -1,9 +1,12 @@
-﻿namespace MonJobs
+﻿using System.Threading.Tasks;
+
+namespace MonJobs
 {
     /// <summary>
     /// Allows you to constantly append results.
     /// </summary>
     public interface IJobReportSerivce
     {
+        Task AddReport(QueueId queue, JobId id, JobReport report);
     }
 }
