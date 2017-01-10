7d2135d41c29c4eed1dadafb9e903d3b763bbdf9
Mitch Ferrer <mitch@lifeafternormal.net>
2017-01-10 11:26:22 -0700

    removing prompt for subscriptions package

--- NugetBuild.cmd
+++ NugetBuild.cmd
@@ -4,7 +4,7 @@ Setlocal EnableDelayedExpansion
 SET /p apiKey=Enter API Key: %=%
 .nuget\nuget setApiKey !apiKey!
 
-SET /p deployMonJobs=Would you like to deploy the Core Project? (y/n) %=%
+SET /p deployMonJobs=Would you like to deploy the Core MonJobs Project? (y/n) %=%
 IF (!deployMonJobs!) EQU (y) (
 	.nuget\nuget pack MonJobs\MonJobs.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 	@ECHO Finished Building: MonJobs
@@ -12,16 +12,6 @@ IF (!deployMonJobs!) EQU (y) (
 	SET /p monJobsVersion=Enter MonJobs Package Version? %=%
 	@ECHO Publishing MonJobs.!monJobsVersion!.nupkg
 	.nuget\nuget push MonJobs.!monJobsVersion!.nupkg
-)
-
-SET /p deploySubscriptions=Would you like to deploy the Subscriptions Project? (y/n) %=%
-IF (!deploySubscriptions!) EQU (y) (
-	.nuget\nuget pack MonJobs.Subscriptions\MonJobs.Subscriptions.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
-	@ECHO Finished Building: MonJobs.Subscriptions
-
-	SET /p subscriptionsVersion=Enter MonJobs Subscriptions Package Version? %=%
-	@ECHO Publishing MonJobs.Subscriptions.!subscriptionsVersion!.nupkg
-	.nuget\nuget push MonJobs.Subscriptions.!subscriptionsVersion!.nupkg
 )
 
 @ECHO on
